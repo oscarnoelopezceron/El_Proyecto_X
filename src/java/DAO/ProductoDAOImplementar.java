59f31a235c66c17ce6c74fd7f3ff41df9c31411f
oscarnoelopezceron <61834003+oscarnoelopezceron@users.noreply.github.com>
2020-06-28 11:42:40 -0600

    editarProductos a un 75%

--- src/java/DAO/ProductoDAOImplementar.java
+++ src/java/DAO/ProductoDAOImplementar.java
@@ -20,13 +20,13 @@ public class ProductoDAOImplementar implements ProductoDAO{
     ConexionBD conn;//La variable conexion
     public ProductoDAOImplementar() {
         //la conexion [ara la definicion de la bd que s econectara por defecto
-        this.conn = FactoryConexionBD.open(FactoryConexionBD.MySQL);
+        //this.conn = FactoryConexionBD.open(FactoryConexionBD.MySQL);
         
     }
 
     @Override
     public List<Producto> Listar() {
-        //this.conn = FactoryConexionBD.open(FactoryConexionBD.MySQL);
+        this.conn = FactoryConexionBD.open(FactoryConexionBD.MySQL);
         
         StringBuilder miSQL = new StringBuilder();
         miSQL.append("SELECT * FROM tb_producto; ");
@@ -63,7 +63,30 @@ public class ProductoDAOImplementar implements ProductoDAO{
 
     @Override
     public Producto editarPro(int id_pro_edit) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.conn = FactoryConexionBD.open(FactoryConexionBD.MySQL);
+        Producto producto = new Producto();
+        Categoria c = new Categoria();
+        StringBuilder miSQL = new StringBuilder();
+        
+        miSQL.append("SELECT * FROM tb_producto WHERE id_producto = ").append(id_pro_edit);
+        try{
+            ResultSet resultadoSQL = this.conn.consultaSQL(miSQL.toString());
+            while(resultadoSQL.next()){
+                 producto.setId_producto(resultadoSQL.getInt("id_producto"));
+                 producto.setNom_producto(resultadoSQL.getString("nom_producto"));
+                 producto.setStock(resultadoSQL.getDouble("stock"));
+                 producto.setPrecio(resultadoSQL.getDouble("precio"));
+                 producto.setUnidad_de_medida(resultadoSQL.getString("unidad_de_medida"));
+                 producto.setEstado_producto(resultadoSQL.getInt("estado_producto"));
+                 c.setNom_categoria(resultadoSQL.getString("nom_categoria"));
+                 producto.setDes_producto(resultadoSQL.getString("des_producto"));
+                 producto.setFecha_entrada(resultadoSQL.getDate("fecha_entrada"));
+            }
+        }catch(Exception e){
+        }finally{
+            this.conn.cerrarConexion();
+        }
+        return producto;
     }
 
     @Override
--- src/java/Test/PruebaPro.java
+++ src/java/Test/PruebaPro.java
@@ -12,6 +12,7 @@ public class PruebaPro {
     public static void main(String[] args) {
         PruebaPro evaluar = new PruebaPro();
         evaluar.listarProductos();
+        evaluar.edidarProductos();
     }
 
     public void listarProductos() {
@@ -33,6 +34,22 @@ public class PruebaPro {
             );
     
         }
+    }
+    
+    public void edidarProductos(){
+        ProductoDAO producto = new ProductoDAOImplementar();
+        Producto pro_edit = producto.editarPro(3);
+        System.out.println("PRODUCTOS MODIFICAR: ");
+        System.out.println("ID: "+pro_edit.getId_producto()+
+                           " NOMBRE: "+pro_edit.getNom_producto()+
+                           " STOSCK: "+pro_edit.getStock()+
+                           " PRECIO: "+pro_edit.getPrecio()+
+                           " ESTADO DEL PRODUCTO: "+pro_edit.getEstado_producto()+
+                           " CATEGORIA: "+pro_edit.getCategoria()+
+                           " DESCIRPCION: "+pro_edit.getDes_producto()+
+                           " FECHA: "+pro_edit.getFecha_entrada()
+                );
+                
     }
     
 }
